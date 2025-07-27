83b7f938437ec8a2a63c927987a06a9b9ab003cb
Pedro Henrique <ph8584774@gmail.com>
2025-07-27 00:04:03 -0300

    Solução com interface e classe abstrata
    Signed-off-by: Pedro Henrique <ph8584774@gmail.com>

--- secao-16/aulas/aula-2-herdar-x-cumprir-contrato/src/Main.java
+++ secao-16/aulas/aula-2-herdar-x-cumprir-contrato/src/Main.java
@@ -1,13 +1,13 @@
+import entities.AbstractShape;
 import entities.Circle;
 import entities.Rectangle;
-import entities.Shape;
 import enums.Color;
 
 public class Main {
     public static void main(String[] args) {
 
-        Shape s1 = new Circle(Color.BLACK, 2.0);
-        Shape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
+        AbstractShape s1 = new Circle(Color.BLACK, 2.0);
+        AbstractShape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
 
         System.out.println("Circle color: " + s1.getColor());
         System.out.println("Circle area: " + String.format("%.3f", s1.area()));

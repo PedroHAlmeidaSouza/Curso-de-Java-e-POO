021974a5e6135a9ba4c11d5c2aa51918a1c1effb
PedroHAlmeidaSouza <ph8584774@gmail.com>
2025-07-30 11:58:44 -0300

    Comparator objeto de expressão lambda sem chaves
    Signed-off-by: PedroHAlmeidaSouza <ph8584774@gmail.com>

--- secao-18/aulas/aula-1-comparator/src/Main.java
+++ secao-18/aulas/aula-1-comparator/src/Main.java
@@ -13,9 +13,7 @@ public class Main {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.50));
 
-        Comparator<Product> comparator = (o1, p2) -> {
-            return o1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-        };
+        Comparator<Product> comparator = (o1, p2) -> o1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 
         list.sort(comparator);
 

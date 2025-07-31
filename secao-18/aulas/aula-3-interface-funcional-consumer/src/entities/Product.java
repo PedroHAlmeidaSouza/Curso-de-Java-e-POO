3c6b806539e6bfcf4b233d3d6221e77a1e5e4462
PedroHAlmeidaSouza <ph8584774@gmail.com>
2025-07-31 15:16:38 -0300

    Interface consumer: Reference method com método não estático e estático
    Signed-off-by: PedroHAlmeidaSouza <ph8584774@gmail.com>

--- secao-18/aulas/aula-3-interface-funcional-consumer/src/entities/Product.java
+++ secao-18/aulas/aula-3-interface-funcional-consumer/src/entities/Product.java
@@ -26,6 +26,14 @@ public class Product {
         this.price = price;
     }
 
+    public static void staticPriceUpdate(Product p) {
+        p.setPrice(p.getPrice() * 1.1);
+    }
+
+    public void nonStaticPriceUpdate() {
+        price = price * 1.1;
+    }
+
     @Override
     public String toString() {
         return "Product{" +

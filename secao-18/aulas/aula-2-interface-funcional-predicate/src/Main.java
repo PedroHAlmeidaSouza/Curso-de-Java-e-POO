3d2640a030bf1d39674a8bc688bd017c3be07c24
PedroHAlmeidaSouza <ph8584774@gmail.com>
2025-07-31 14:55:29 -0300

    Expressão lambda declarada
    Signed-off-by: PedroHAlmeidaSouza <ph8584774@gmail.com>

--- secao-18/aulas/aula-2-interface-funcional-predicate/src/Main.java
+++ secao-18/aulas/aula-2-interface-funcional-predicate/src/Main.java
@@ -4,6 +4,7 @@ import util.ProductPredicate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,7 +17,9 @@ public class Main {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.removeIf(Product::nonStaticProductPredicate);
+        Predicate<Product> predicate = p -> p.getPrice() >= 100.00;
+
+        list.removeIf(predicate);
 
         for (Product p : list) {
             System.out.println(p);

a0a035b32237878f181fa7a056cbbcc77db4d166
PedroHAlmeidaSouza <ph8584774@gmail.com>
2025-07-31 17:48:56 -0300

    Problema exemplo solução inicial sem predicado
    Signed-off-by: PedroHAlmeidaSouza <ph8584774@gmail.com>

---
+++ secao-18/aulas/aula-5-funcoes-que-recebem-funcoes/src/service/ProductService.java
@@ -0,0 +1,18 @@
+package service;
+
+import entities.Product;
+
+import java.util.List;
+
+public class ProductService {
+
+    public double filteredSum(List<Product> list) {
+        double sum = 0.0;
+        for (Product p : list) {
+            if (p.getName().startsWith("T")) {
+                sum += p.getPrice();
+            }
+        }
+        return sum;
+    }
+}

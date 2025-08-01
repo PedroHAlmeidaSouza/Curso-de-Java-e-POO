078da8b6612215df74300d363dbadc5c373c88b9
PedroHAlmeidaSouza <ph8584774@gmail.com>
2025-08-01 17:23:45 -0300

    Conexão com o banco de dados
    Signed-off-by: PedroHAlmeidaSouza <ph8584774@gmail.com>

---
+++ secao-19/aulas/aula-1-JDBC/src/db/DB.java
@@ -0,0 +1,47 @@
+package db;
+
+import db.exceptions.DbException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Properties props = loadProperties();
+                conn = DriverManager.getConnection(props.getProperty("dburl"), props.getProperty("user"), props.getProperty("password"));
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        } catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+}

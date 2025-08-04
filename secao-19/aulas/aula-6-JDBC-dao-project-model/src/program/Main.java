e0f5789219bdd12c95808bed84c86150201eca26
PedroHAlmeidaSouza <ph8584774@gmail.com>
2025-08-04 16:36:23 -0300

    SellerDaoJDBC e DaoFactory
    Signed-off-by: PedroHAlmeidaSouza <ph8584774@gmail.com>

--- secao-19/aulas/aula-6-JDBC-dao-project-model/src/program/Main.java
+++ secao-19/aulas/aula-6-JDBC-dao-project-model/src/program/Main.java
@@ -1,5 +1,7 @@
 package program;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Main {
         Department obj = new Department(1, "Books");
 
         Seller obj2 = new Seller(1, "Pedro", "pedro@gmail.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(obj);
         System.out.println(obj2);

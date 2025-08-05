4411471cf4f3e15da95b7477e425a7e3c9f39609
PedroHAlmeidaSouza <ph8584774@gmail.com>
2025-08-05 11:09:17 -0300

    Implementando findByDepartment apontando para o mesmo Seller
    Signed-off-by: PedroHAlmeidaSouza <ph8584774@gmail.com>

--- secao-19/aulas/aula-6-JDBC-dao-project-model/src/model/dao/impl/SellerDaoJDBC.java
+++ secao-19/aulas/aula-6-JDBC-dao-project-model/src/model/dao/impl/SellerDaoJDBC.java
@@ -10,7 +10,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SellerDaoJDBC implements SellerDao {
 
@@ -38,6 +41,61 @@ public class SellerDaoJDBC implements SellerDao {
     }
 
     @Override
+    public List<Seller> findByDepartment(Department department) {
+        // Representa o comando SQL pré-compilado
+        PreparedStatement st = null;
+
+        // Representa o resultado de uma consulta SQL
+        ResultSet rs = null;
+
+        try {
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as DepName "
+                            + "FROM seller INNER JOIN department "
+                            + "ON seller.DepartmentId = department.Id "
+                            + "WHERE DepartmentId = ? "
+                            + "ORDER BY Name");
+
+            // Define o valor Id no 1o ? da query como um inteiro, usado para indicar o Id do department que será consultado
+            st.setInt(1, department.getId());
+
+            // Executa a query no banco de dados
+            rs = st.executeQuery();
+
+            // Gera um HashMap para não gerar duplicidade ao apontar para o Seller associado
+            List<Seller> list = new ArrayList<>();
+            Map<Integer, Department> map = new HashMap<>();
+            
+            // Itera sobre os resultado encontrados no ResultSet
+            while (rs.next()) {
+                // Valida se no HashMap não existe um valor de DepartmentId já existente, caso não exista ele adiciona no Map
+                Department dep = map.get(rs.getInt("DepartmentId"));
+
+                // Se dep for igual a nulo significa que ele nao existe na lista do HashMap, ou seja, deve ser instanciado e adicionado a mesma posteriormente
+                if (dep == null) {
+                    // Somente para cada valor encontrado ele instancia um novo Department através do ResultSet
+                    dep = instantiateDepartment(rs);
+                    map.put(rs.getInt("DepartmentId"), dep);
+                }
+
+                Seller obj = instantiateSeller(rs, dep);
+
+                // Adiciona os objetos Seller encontrados à lista de retorno
+                list.add(obj);
+            }
+            return list;
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            // Fecha as conexões
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    @Override
     public Seller findById(Integer id) {
         // Representa o comando SQL pré-compilado
         PreparedStatement st = null;
@@ -52,13 +110,13 @@ public class SellerDaoJDBC implements SellerDao {
                             + "ON seller.DepartmentId = department.Id "
                             + "WHERE seller.Id = ?");
 
-            // Define o valor Id no 1o ? da query como um inteiro, usado para indicar o Id do departamento que será consultado
+            // Define o valor Id no 1o ? da query como um inteiro, usado para indicar o Id do Seller que será consultado
             st.setInt(1, id);
 
             // Executa a query no banco de dados
             rs = st.executeQuery();
 
-            // Itera sobre as linhas do ResultSet instânciando os departamentos encontrados
+            // Valida a linha de posição 1 encontrada o ResultSet se é nula, caso contrario instância o Seller e Department associado
             if (rs.next()) {
                 Department dep = instantiateDepartment(rs);
                 return instantiateSeller(rs, dep);
--- secao-19/aulas/aula-6-JDBC-dao-project-model/src/program/Main.java
+++ secao-19/aulas/aula-6-JDBC-dao-project-model/src/program/Main.java
@@ -2,7 +2,10 @@ package program;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,10 +13,19 @@ public class Main {
         // Retorna uma instanciação da minha implementação que me da acesso as operações
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        // Realizo uma busca por ID e retorno um objeto Seller
+        // Realizo uma busca por sellerID e retorno um objeto Seller
         System.out.println("=== TESTE 1: Seller findById ===");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+
+        System.out.println();
+
+        System.out.println("=== TESTE 2: Seller findByDepartId ===");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
+
     }
 }

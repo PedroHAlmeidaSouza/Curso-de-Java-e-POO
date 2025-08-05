d6d3ef02ad546ccb6262401f84066a8ef6d99200
PedroHAlmeidaSouza <ph8584774@gmail.com>
2025-08-05 17:17:24 -0300

    Implementação completa do DepartmentDao
    Signed-off-by: PedroHAlmeidaSouza <ph8584774@gmail.com>

---
+++ secao-19/aulas/aula-6-JDBC-dao-project-model/src/program/MainDepartment.java
@@ -0,0 +1,61 @@
+package program;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class MainSeller {
+    public static void main(String[] args) {
+
+        // Retorna uma instanciação da minha implementação que me da acesso as operações
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println();
+        // Realizo uma busca por sellerID e retorno um objeto Seller
+        System.out.println("=== TESTE 1: Seller findById ===");
+        Seller seller = sellerDao.findById(1);
+        System.out.println(seller);
+
+        System.out.println();
+        // Realiza uma busca por DepartmentId
+        System.out.println("=== TESTE 2: Seller findByDepartId ===");
+        Department department = new Department(2, null);
+        sellerDao.findByDepartment(department).forEach(System.out::println);
+
+        System.out.println();
+        // Retorna todos as Sellers e Departments associados
+        System.out.println("=== TESTE 3: Seller findAll ===");
+        sellerDao.findAll().forEach(System.out::println);
+
+        System.out.println();
+        // Insere um novo Saller
+        System.out.println("=== TESTE 4: Seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println();
+        // Atualiza um Seller
+        System.out.println("=== TESTE 5: Seller update ===");
+        seller = sellerDao.findById(seller.getId());
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println();
+        // Deleta um Seller
+        System.out.println("=== TESTE 6: Seller delete ===");
+        System.out.print("Enter the seller id to be deleted: ");
+        sellerDao.deleteById(sc.nextInt());
+        System.out.println("Delete completed!");
+
+        sc.close();
+
+    }
+}

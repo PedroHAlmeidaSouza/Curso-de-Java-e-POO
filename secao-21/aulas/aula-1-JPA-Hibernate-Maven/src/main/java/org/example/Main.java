79ce2ce70a07908e976f55aa7f57735883e6f891
PedroHAlmeidaSouza <ph8584774@gmail.com>
2025-08-06 17:07:18 -0300

    JPA: recuperação e deleção de entidades
    Signed-off-by: PedroHAlmeidaSouza <ph8584774@gmail.com>

--- secao-21/aulas/aula-1-JPA-Hibernate-Maven/src/main/java/org/example/Main.java
+++ secao-21/aulas/aula-1-JPA-Hibernate-Maven/src/main/java/org/example/Main.java
@@ -9,35 +9,23 @@ import javax.persistence.Persistence;
 public class Main {
     public static void main(String[] args) {
 
-        // Instancia objetos Pessoa sem ID (banco irá gerar)
-        Pessoa pessoa1 = new Pessoa(null, "Maria Silva", "maria@email.com");
-        Pessoa pessoa2 = new Pessoa(null, "João Santos", "joao@email.com");
-        Pessoa pessoa3 = new Pessoa(null, "Ana Oliveira", "ana@email.com");
-
         // Cria a fábrica de conexões com o banco de dados
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("exemplo-jpa");
-
         // Cria o gerenciador de entidades
         EntityManager manager = factory.createEntityManager();
 
-        // Inicia a transação com o banco de dados
         manager.getTransaction().begin();
 
-        // Salva os objetos no banco de dados
-        manager.persist(pessoa1);
-        manager.persist(pessoa2);
-        manager.persist(pessoa3);
+        // Para remover uma entidade é necessário primeiro recuperá-la do banco para que ela esteja em estado de "monitorada"
+        Pessoa p = manager.find(Pessoa.class, 2);
+        manager.remove(p);
 
-        // Confirma a transação
         manager.getTransaction().commit();
+
+        System.out.println(p);
 
         // Fecha as conexões do manager e factory com o banco
         manager.close();
         factory.close();
-
-        // Imprime os objetos já salvos com os IDs gerados
-        System.out.println(pessoa1);
-        System.out.println(pessoa2);
-        System.out.println(pessoa3);
     }
 }
